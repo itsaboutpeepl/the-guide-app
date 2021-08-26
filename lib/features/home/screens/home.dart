901acc880d928225cbcde5f43fb01a7c9fff15f4
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2021-08-26 14:17:35 +0100

    button

--- lib/features/home/screens/home.dart
+++ lib/features/home/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:peepl/features/shared/widgets/primary_button.dart';
 import 'package:peepl/models/app_state.dart';
 import 'package:peepl/redux/actions/cash_wallet_actions.dart';
 import 'package:peepl/redux/viewsmodels/home.dart';
@@ -54,63 +57,151 @@ class HomeScreen extends StatelessWidget {
                         ),
                         builder: (BuildContext context) {
                           return Container(
-                            height: height * 0.5,
+                            height: height * 0.6,
                             color: Colors.transparent,
-                            child: Center(
-                              child: Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text('Peepl.pay'),
-                                      ),
-                                      Spacer(),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: IconButton(
-                                          icon: Icon(
-                                            Icons.cancel_outlined,
-                                            color: Colors.blueGrey,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Center(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                            left: 8.0,
+                                            right: 8.0,
+                                            top: 8.0,
                                           ),
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
+                                          child: Text('Peepl.pay'),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Container(
-                                          color: Colors.red,
-                                          child: Text("Company Logo"),
-                                          height: height * 0.1,
-                                          width: width * 0.2,
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          children: [
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: Text("Company Name"),
+                                        Spacer(),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                            left: 8.0,
+                                            right: 8.0,
+                                            top: 8.0,
+                                          ),
+                                          child: IconButton(
+                                            icon: Icon(
+                                              Icons.cancel_outlined,
+                                              color: Colors.blueGrey,
                                             ),
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: Text("Pay with Peepl.Pay"),
-                                            )
-                                          ],
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                          ),
                                         ),
-                                      )
-                                    ],
-                                  ),
-                                ],
+                                      ],
+                                    ),
+                                    Divider(),
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Container(
+                                            color: Colors.red,
+                                            child: Text("Company Logo"),
+                                            height: height * 0.1,
+                                            width: width * 0.2,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  "Company Name",
+                                                  style: TextStyle(
+                                                    fontSize: 18,
+                                                  ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  "Pay with Peepl.Pay",
+                                                  style: TextStyle(
+                                                      color: Colors.blueGrey,
+                                                      fontSize: 15),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    Divider(),
+                                    Text(
+                                      "To pay send amount to the address:",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.blueGrey,
+                                      ),
+                                    ),
+                                    Text("Amount"),
+                                    TextFormField(
+                                      enabled: false,
+                                      decoration: InputDecoration(
+                                        hintText: '50 GBPx',
+                                        contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 20.0, vertical: 5.0),
+                                        filled: true,
+                                        floatingLabelBehavior:
+                                            FloatingLabelBehavior.never,
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                    Text("Address"),
+                                    TextFormField(
+                                      enabled: false,
+                                      decoration: InputDecoration(
+                                        hintText:
+                                            '0xc0ffee254729296a45a3885639AC7E10F9d54979',
+                                        contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 20.0, vertical: 5.0),
+                                        filled: true,
+                                        floatingLabelBehavior:
+                                            FloatingLabelBehavior.never,
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                        suffixIcon: IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(
+                                            Icons.copy_all_outlined,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        PrimaryButton(
+                                          opacity: 1,
+                                          // labelFontWeight: FontWeight.normal,
+                                          label: "Pay",
+                                          onPressed: () {},
+
+                                          width: width * 0.3,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           );
