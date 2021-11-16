d07895e2e81c1e5b8a6fdec11cb4afe159fdbfe6
Hussain Surti <surti.huss@gmail.com>
2021-11-16 13:08:15 +0400

    changed featured post to get a list of posts instead of a single post

--- lib/services/apis/news.dart
+++ lib/services/apis/news.dart
@@ -32,4 +32,50 @@ class NewsService {
       content: results[0]['content'],
     );
   }
+
+  Future<List<BlogArticle>> featuredArticles() async {
+    Response response = await dio.get(
+        'top-headlines?country=us&category=business&apiKey=a920ad97e9fc4e84933b96be2f3a1ad3');
+
+    List<dynamic> results = response.data['articles'] as List;
+
+    List<BlogArticle> articles = [];
+
+    for (var i = 0; i < 3; i++) {
+      articles.add(BlogArticle(
+        title: results[i]['title'] ?? "",
+        imageURL: results[i]['urlToImage'] ??
+            "https://www.cityam.com/wp-content/uploads/2021/07/CAMD-G89-1024-GBrown-copy-1.jpg",
+        category: [results[i]['author'] ?? "Hussain Surti"],
+        postID: 23132,
+        postURL: results[i]['url'] ??
+            "https://www.cityam.com/wp-content/uploads/2021/07/CAMD-G89-1024-GBrown-copy-1.jpg",
+        content: results[i]['content'] ?? "Lorem imsum io",
+      ));
+    }
+
+    // results.forEach((element) {
+    //   articles.add(BlogArticle(
+    //     title: element['title'] ?? "",
+    //     imageURL: element['urlToImage'] ??
+    //         "https://www.cityam.com/wp-content/uploads/2021/07/CAMD-G89-1024-GBrown-copy-1.jpg",
+    //     category: [element['author'] ?? "Hussain Surti"],
+    //     postID: 23132,
+    //     postURL: element['url'] ??
+    //         "https://www.cityam.com/wp-content/uploads/2021/07/CAMD-G89-1024-GBrown-copy-1.jpg",
+    //     content: element['content'] ?? "Lorem imsum io",
+    //   ));
+    // });
+
+    return articles;
+
+    // return BlogArticle(
+    //   postID: 21312,
+    //   imageURL: results[0]['urlToImage'],
+    //   title: results[0]['title'],
+    //   postURL: results[0]['url'],
+    //   category: category,
+    //   content: results[0]['content'],
+    // );
+  }
 }
