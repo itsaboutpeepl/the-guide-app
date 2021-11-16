832df9ba055ed2ed7a9c2f384c6de342022718f6
Hussain Surti <surti.huss@gmail.com>
2021-11-16 13:13:25 +0400

    refactored to take data from widget rather than store connector

--- lib/features/guideHome/widgets/featuredPost.dart
+++ lib/features/guideHome/widgets/featuredPost.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:peepl/features/guideHome/helpers/detailArticleBottomModal.dart';
-import 'package:peepl/models/app_state.dart';
-import 'package:peepl/redux/viewsmodels/featuredPost.dart';
+import 'package:peepl/models/articles/blogArticle.dart';
 
 class FeaturedPost extends StatefulWidget {
-  const FeaturedPost({Key? key}) : super(key: key);
+  const FeaturedPost({Key? key, required this.index, required this.article})
+      : super(key: key);
+
+  final int index;
+  final BlogArticle article;
 
   @override
   _FeaturedPostState createState() => _FeaturedPostState();
@@ -15,86 +17,88 @@ class FeaturedPost extends StatefulWidget {
 class _FeaturedPostState extends State<FeaturedPost> {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, FeaturedPostViewModel>(
-      distinct: true,
-      converter: FeaturedPostViewModel.fromStore,
-      builder: (_, viewmodel) => featuredPost(_, viewmodel),
-    );
+    return featuredPost(context, widget.article);
   }
 }
 
-Widget featuredPost(BuildContext context, FeaturedPostViewModel viewModel) {
-  return SliverPadding(
-    padding: EdgeInsets.all(15),
-    sliver: SliverToBoxAdapter(
-      child: InkWell(
-        onTap: () => {
-          // context.router
-          //     .push(DetailBlogArticle(blogArticle: viewModel.blogArticle))
-          showBarModalBottomSheet(
-            useRootNavigator: true,
-            backgroundColor: Colors.cyan,
-            context: context,
-            builder: (context) =>
-                DetailArticleBottomModel(articleData: viewModel.blogArticle),
-          )
-        },
-        child: Card(
-          color: Colors.grey[100],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
+Widget featuredPost(BuildContext context, BlogArticle article) {
+  return Padding(
+    padding: EdgeInsets.only(right: 15, left: 15, top: 15, bottom: 55),
+    child: InkWell(
+      onTap: () => {
+        showBarModalBottomSheet(
+          useRootNavigator: true,
+          backgroundColor: Colors.cyan,
+          context: context,
+          builder: (context) => DetailArticleBottomModel(articleData: article),
+        )
+      },
+      child: Card(
+        color: Colors.grey[100],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+              child: Container(
+                height: 200,
+                width: double.infinity,
                 child: Image.network(
-                  viewModel.blogArticle.imageURL,
+                  article.imageURL,
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 10),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Text(
-                  viewModel.blogArticle.title,
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 23),
-                ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                article.title,
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 23),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-                child: Row(
-                  children: [
-                    Text(viewModel.blogArticle.category[0]),
-                    Icon(
-                      Icons.circle,
-                      size: 6,
-                      color: Colors.grey[500],
-                    ),
-                    Text(
-                      " 3 min ago",
-                      style: TextStyle(color: Colors.grey[500]),
-                    )
-                  ],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+              child: Row(
+                children: [
+                  Text(article.category[0]),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(
+                    Icons.circle,
+                    size: 6,
+                    color: Colors.grey[500],
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "3 min ago",
+                    style: TextStyle(color: Colors.grey[500]),
+                  )
+                ],
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Text(
-                  viewModel.blogArticle.content,
-                  style: TextStyle(fontSize: 15),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                article.content,
+                style: TextStyle(fontSize: 15),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 4,
               ),
-              SizedBox(height: 20),
-            ],
-          ),
+            ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
     ),
