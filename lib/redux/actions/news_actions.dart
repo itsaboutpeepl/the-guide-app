2708ab27439414886d6caaddab56068a8f7f34e3
Hussain Surti <surti.huss@gmail.com>
2021-11-22 13:11:12 +0400

    Actions written for every widget on homepage

--- lib/redux/actions/news_actions.dart
+++ lib/redux/actions/news_actions.dart
@@ -1,4 +1,8 @@
 import 'package:peepl/models/articles/blogArticle.dart';
+import 'package:peepl/models/articles/categoryArticles.dart';
+import 'package:peepl/models/articles/directory.dart';
+import 'package:peepl/models/articles/events.dart';
+import 'package:peepl/models/articles/videoArticle.dart';
 import 'package:peepl/services.dart';
 import 'package:peepl/utils/log/log.dart';
 import 'package:redux_thunk/redux_thunk.dart';
@@ -9,6 +13,30 @@ class UpdateFeaturedPost {
   final List<BlogArticle> listOfFeaturedArticles;
 
   UpdateFeaturedPost({required this.listOfFeaturedArticles});
+}
+
+class UpdateCategoryList {
+  final List<CategoryArticles> categoryList;
+
+  UpdateCategoryList({required this.categoryList});
+}
+
+class UpdateFeaturedVideos {
+  final List<VideoArticle> featuredVideos;
+
+  UpdateFeaturedVideos({required this.featuredVideos});
+}
+
+class UpdateEventsList {
+  final List<Events> eventsList;
+
+  UpdateEventsList({required this.eventsList});
+}
+
+class UpdateDirectoryList {
+  final List<Directory> directoryList;
+
+  UpdateDirectoryList({required this.directoryList});
 }
 
 ThunkAction fetchFeaturedPost() {
@@ -23,6 +51,86 @@ ThunkAction fetchFeaturedPost() {
         e,
         stackTrace: s,
         hint: 'ERROR - fetchFeaturedPost $e',
+      );
+    }
+  };
+}
+
+ThunkAction fetchCategoryList() {
+  return (Store store) async {
+    try {
+      List<Map> categoryNames = await newsService.categoryNames();
+      List<CategoryArticles> categoryList = [];
+
+      categoryNames.forEach(
+        (element) async {
+          categoryList.add(
+            CategoryArticles(
+              categoryID: element[0],
+              categoryName: element[1],
+              articleList: await newsService.articlesByCategoryID(
+                element[0],
+              ),
+            ),
+          );
+        },
+      );
+
+      store.dispatch(UpdateCategoryList(categoryList: categoryList));
+    } catch (e, s) {
+      log.error('ERROR - fetchCategoryList $e');
+      await Sentry.captureException(
+        e,
+        stackTrace: s,
+        hint: 'ERROR - fetchCategoryList $e',
+      );
+    }
+  };
+}
+
+ThunkAction fetchFeaturedVideos() {
+  return (Store store) async {
+    try {
+      List<VideoArticle> videoArticles = await newsService.featuredVideos();
+      store.dispatch(UpdateFeaturedVideos(featuredVideos: videoArticles));
+    } catch (e, s) {
+      log.error('ERROR - fetchFeaturedVideos $e');
+      await Sentry.captureException(
+        e,
+        stackTrace: s,
+        hint: 'ERROR - fetchFeaturedVideos $e',
+      );
+    }
+  };
+}
+
+ThunkAction fetchEventsList() {
+  return (Store store) async {
+    try {
+      List<Events> eventsList = await newsService.eventsList();
+      store.dispatch(UpdateEventsList(eventsList: eventsList));
+    } catch (e, s) {
+      log.error('ERROR - fetchEventsList $e');
+      await Sentry.captureException(
+        e,
+        stackTrace: s,
+        hint: 'ERROR - fetchEventsList $e',
+      );
+    }
+  };
+}
+
+ThunkAction fetchDirectoryList() {
+  return (Store store) async {
+    try {
+      List<Directory> directoryList = await newsService.directoryList();
+      store.dispatch(UpdateDirectoryList(directoryList: directoryList));
+    } catch (e, s) {
+      log.error('ERROR - fetchDirectoryList $e');
+      await Sentry.captureException(
+        e,
+        stackTrace: s,
+        hint: 'ERROR - fetchDirectoryList $e',
       );
     }
   };
