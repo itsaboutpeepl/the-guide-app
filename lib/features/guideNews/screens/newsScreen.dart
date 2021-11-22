92a6e64835276c48bcf33da2b1f418d73ba6afb7
Hussain Surti <surti.huss@gmail.com>
2021-11-22 15:54:17 +0400

    added new news screen for holding category tabs and views

---
+++ lib/features/guideNews/screens/newsScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.cyan,
+      child: Center(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
