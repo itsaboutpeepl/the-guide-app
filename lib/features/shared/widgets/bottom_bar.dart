9741d7057c460641079ab2bbedc2793fdd19057a
Hussain Surti <surti.huss@gmail.com>
2021-11-23 01:52:24 +0400

    Refactored nav bar to fit new widgets

--- lib/features/shared/widgets/bottom_bar.dart
+++ lib/features/shared/widgets/bottom_bar.dart
@@ -1,7 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:peepl/constants/theme.dart';
+import 'package:peepl/features/shared/widgets/motionTabBar.dart';
 import 'package:peepl/generated/l10n.dart';
 import 'package:peepl/models/app_state.dart';
 import 'package:peepl/redux/viewsmodels/main_page.dart';
@@ -14,27 +15,27 @@ class BottomBar extends StatelessWidget {
     this.tabsRouter,
   );
 
-  BottomNavigationBarItem bottomBarItem(
-    String title,
-    String imgSvg,
-  ) =>
-      BottomNavigationBarItem(
-        icon: Padding(
-          padding: EdgeInsets.only(top: 5, bottom: 3),
-          child: SvgPicture.asset(
-            'assets/images/$imgSvg\.svg',
-          ),
-        ),
-        activeIcon: Padding(
-          padding: EdgeInsets.only(top: 5, bottom: 3),
-          child: SvgPicture.asset(
-            'assets/images/$imgSvg\_selected.svg',
-            width: 26,
-            height: 26,
-          ),
-        ),
-        label: title,
-      );
+  // BottomNavigationBarItem bottomBarItem(
+  //   String title,
+  //   String imgSvg,
+  // ) =>
+  //     BottomNavigationBarItem(
+  //       icon: Padding(
+  //         padding: EdgeInsets.only(top: 5, bottom: 3),
+  //         child: SvgPicture.asset(
+  //           'assets/images/$imgSvg\.svg',
+  //         ),
+  //       ),
+  //       activeIcon: Padding(
+  //         padding: EdgeInsets.only(top: 5, bottom: 3),
+  //         child: SvgPicture.asset(
+  //           'assets/images/$imgSvg\_selected.svg',
+  //           width: 26,
+  //           height: 26,
+  //         ),
+  //       ),
+  //       label: title,
+  //     );
 
   @override
   Widget build(BuildContext context) {
@@ -47,48 +48,75 @@ class BottomBar extends StatelessWidget {
         store.dispatch(startFetchTokensBalances());
         store.dispatch(updateTokensPrices());
       },
-      builder: (_, vm) => BottomNavigationBar(
-        onTap: (int activeIndex) {
+      builder: (_, vm) => MotionTabBar(
+        tabIconColor: flexColorSchemeLight.primary,
+        textStyle: TextStyle(color: flexColorSchemeLight.primary),
+        tabSelectedColor: flexColorSchemeLight.primary,
+        initialSelectedTab: I10n.of(context).home,
+        onTabItemSelected: (int activeIndex) {
           if (activeIndex == tabsRouter.activeIndex) {
             context.router.popTop();
           } else {
             tabsRouter.setActiveIndex(activeIndex);
           }
         },
-        selectedItemColor: Color(0xFF292929),
-        selectedFontSize: 13,
-        unselectedFontSize: 13,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: tabsRouter.activeIndex,
-        backgroundColor: Theme.of(context).bottomAppBarColor,
-        showUnselectedLabels: true,
-        //To do: Add internationalisation
-        items: [
-          bottomBarItem(I10n.of(context).home, 'home'),
-          bottomBarItem("News", "news"),
-          bottomBarItem('Wallet', 'wallet'),
-          //bottomBarItem('Shop', 'order'),
-          //bottomBarItem('Top Up', 'topup'),
-          // bottomBarItem('Tips', 'tips'),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 3),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/anom.png'),
-                radius: 13,
-              ),
-            ),
-            activeIcon: Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 3),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/anom.png'),
-                radius: 14,
-              ),
-            ),
-            label: I10n.of(context).account,
-          )
+        labels: [
+          I10n.of(context).home,
+          "News",
+          "Wallet",
+          I10n.of(context).account,
+        ],
+        icons: [
+          Icons.home,
+          Icons.article,
+          Icons.account_balance_wallet,
+          Icons.person_outline,
         ],
       ),
     );
   }
 }
+
+
+// BottomNavigationBar(
+//         onTap: (int activeIndex) {
+//           if (activeIndex == tabsRouter.activeIndex) {
+//             context.router.popTop();
+//           } else {
+//             tabsRouter.setActiveIndex(activeIndex);
+//           }
+//         },
+//         selectedItemColor: Color(0xFF292929),
+//         selectedFontSize: 13,
+//         unselectedFontSize: 13,
+//         type: BottomNavigationBarType.fixed,
+//         currentIndex: tabsRouter.activeIndex,
+//         backgroundColor: Theme.of(context).bottomAppBarColor,
+//         showUnselectedLabels: true,
+//         //To do: Add internationalisation
+//         items: [
+//           bottomBarItem(I10n.of(context).home, 'home'),
+//           bottomBarItem("News", "news"),
+//           bottomBarItem('Wallet', 'wallet'),
+//           //bottomBarItem('Shop', 'order'),
+//           //bottomBarItem('Top Up', 'topup'),
+//           // bottomBarItem('Tips', 'tips'),
+//           BottomNavigationBarItem(
+//             icon: Padding(
+//               padding: EdgeInsets.only(top: 5, bottom: 3),
+//               child: CircleAvatar(
+//                 backgroundImage: AssetImage('assets/images/anom.png'),
+//                 radius: 13,
+//               ),
+//             ),
+//             activeIcon: Padding(
+//               padding: EdgeInsets.only(top: 5, bottom: 3),
+//               child: CircleAvatar(
+//                 backgroundImage: AssetImage('assets/images/anom.png'),
+//                 radius: 14,
+//               ),
+//             ),
+//             label: I10n.of(context).account,
+//           )
+//         ],
+//       ),
