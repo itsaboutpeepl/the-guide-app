8dd8581feb39ca23d68ed906d4141add1e7eb3f2
Hussain Surti <surti.huss@gmail.com>
2021-11-23 23:22:36 +0400

    wrote new UI for newsScreen, reduxed (needs infinite scroll)

--- lib/features/guideNews/screens/newsScreen.dart
+++ lib/features/guideNews/screens/newsScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:peepl/features/guideHome/widgets/categoryArticle.dart';
+import 'package:peepl/features/guideNews/widgets/categoryArticlesList.dart';
 import 'package:peepl/models/app_state.dart';
 import 'package:peepl/redux/viewsmodels/newsScreen.dart';
 
@@ -9,51 +9,62 @@ class NewsScreen extends StatefulWidget {
   State<NewsScreen> createState() => _NewsScreenState();
 }
 
-class _NewsScreenState extends State<NewsScreen> {
+class _NewsScreenState extends State<NewsScreen>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, NewsScreenViewModel>(
       distinct: true,
-      onInit: (store) => {},
+      onInit: (store) => {
+        _tabController = TabController(
+            length: store.state.newsState.categoryNames.length, vsync: this)
+      },
       converter: NewsScreenViewModel.fromStore,
-      builder: (_, viewmodel) => DefaultTabController(
-        length: viewmodel.tabNames.length,
-        child: Scaffold(
-          appBar: AppBar(
-            flexibleSpace: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TabBar(
-                  isScrollable: true,
-                  labelColor: Colors.white,
-                  tabs: viewmodel.tabNames
-                      .map(
-                        (String tabName) => Tab(
-                          child: Text(tabName),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ],
-            ),
+      builder: (_, viewmodel) => Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                labelColor: Colors.white,
+                tabs: viewmodel.tabNames
+                    .map(
+                      (String tabName) => Tab(
+                        child: Text(tabName),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
           ),
-          body: TabBarView(
-            children: viewmodel.categoryArticles
-                .map(
-                  (e) => ListView.separated(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (_, index) => CategoryArticle(),
-                    separatorBuilder: (_, index) => Padding(
-                      padding: EdgeInsets.only(bottom: 20),
-                    ),
-                    itemCount: e.articleList.length,
-                  ),
-                )
-                .toList(),
-          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            //add X number of screens for the tabs
+            for (var item in viewmodel.articles) CategoryArticlesList(),
+          ],
         ),
       ),
     );
   }
 }
+
+
+// viewmodel.articles
+//               .map(
+//                 (e) => ListView.separated(
+//                   padding: EdgeInsets.symmetric(vertical: 10),
+//                   scrollDirection: Axis.vertical,
+//                   itemBuilder: (_, index) => CategoryArticle(),
+//                   separatorBuilder: (_, index) => Padding(
+//                     padding: EdgeInsets.only(bottom: 20),
+//                   ),
+//                   itemCount: e.articleList.length,
+//                 ),
+//               )
+//               .toList(),
