644cd29d7d24d5ea39487d4066c4fbbccfc0c246
Hussain Surti <surti.huss@gmail.com>
2021-11-25 13:16:07 +0400

    removed placeholder text, now uses model to build

--- lib/features/guideNews/widgets/singleCategoryArticle.dart
+++ lib/features/guideNews/widgets/singleCategoryArticle.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:peepl/models/articles/blogArticle.dart';
 
-class CategoryArticle extends StatelessWidget {
-  const CategoryArticle({Key? key}) : super(key: key);
+class SingleCategoryArticle extends StatelessWidget {
+  final BlogArticle article;
+  const SingleCategoryArticle({Key? key, required this.article})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +23,12 @@ class CategoryArticle extends StatelessWidget {
             child: Column(
               children: [
                 Text(
-                  "City Council release statement following news that Liverpool is to be given £22m of Government funding",
+                  article.title,
                   style: TextStyle(fontWeight: FontWeight.w900, fontSize: 17),
                 ),
                 SizedBox(height: 5),
                 Text(
-                  "Going out as a group for a meal or drink for a special occasion is great, but sometimes you want somewhere that’s exclusively for you, your family and friends.",
+                  article.content,
                 ),
               ],
             ),
@@ -39,7 +42,7 @@ class CategoryArticle extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(5)),
                 child: Image.network(
-                  "https://www.cityam.com/wp-content/uploads/2021/07/CAMD-G89-1024-GBrown-copy-1.jpg",
+                  article.imageURL,
                   fit: BoxFit.cover,
                 ),
               ),
