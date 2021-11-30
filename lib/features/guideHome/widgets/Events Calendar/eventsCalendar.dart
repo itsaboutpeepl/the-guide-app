40b0665f2ec1fad7b7f4f4e95340bf3dbac573a9
Hussain Surti <surti.huss@gmail.com>
2021-11-30 19:11:14 +0400

    refactored to add store connector

--- lib/features/guideHome/widgets/Events Calendar/eventsCalendar.dart
+++ lib/features/guideHome/widgets/Events Calendar/eventsCalendar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:peepl/features/guideHome/widgets/Events%20Calendar/singleEventItem.dart';
+import 'package:peepl/models/app_state.dart';
+import 'package:peepl/redux/viewsmodels/eventsCalendar.dart';
 
 class EventCalendar extends StatefulWidget {
   const EventCalendar({Key? key}) : super(key: key);
@@ -11,24 +14,30 @@ class EventCalendar extends StatefulWidget {
 class _EventCalendarState extends State<EventCalendar> {
   @override
   Widget build(BuildContext context) {
-    return SliverPadding(
-      padding: EdgeInsets.symmetric(horizontal: 10.0),
-      sliver: SliverToBoxAdapter(
-        child: Card(
-            color: Theme.of(context).primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.24,
-              child: ListView.builder(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  itemBuilder: (context, index) => SingleEventItem(),
-                  itemCount: 2),
-            )),
-      ),
+    return StoreConnector<AppState, EventsCalendarViewModel>(
+      distinct: true,
+      converter: EventsCalendarViewModel.fromStore,
+      builder: (_, viewmodel) {
+        return SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          sliver: SliverToBoxAdapter(
+            child: Card(
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.24,
+                  child: ListView.builder(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      itemBuilder: (context, index) => SingleEventItem(),
+                      itemCount: 2),
+                )),
+          ),
+        );
+      },
     );
   }
 }
