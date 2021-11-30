d55159521a9b5e08bcf94d20e90ed50332ad23e5
Hussain Surti <surti.huss@gmail.com>
2021-11-30 19:09:30 +0400

    Refactored to fit other widget structures

--- lib/features/guideHome/widgets/Events Calendar/eventsCalendar.dart
+++ lib/features/guideHome/widgets/Events Calendar/eventsCalendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peepl/features/guideHome/widgets/Events%20Calendar/singleEventItem.dart';
 
 class EventCalendar extends StatefulWidget {
   const EventCalendar({Key? key}) : super(key: key);
@@ -24,128 +25,10 @@ class _EventCalendarState extends State<EventCalendar> {
               height: MediaQuery.of(context).size.height * 0.24,
               child: ListView.builder(
                   padding: EdgeInsets.symmetric(vertical: 10),
-                  itemBuilder: (context, index) =>
-                      eventItemBuilder(context, index),
+                  itemBuilder: (context, index) => SingleEventItem(),
                   itemCount: 2),
             )),
       ),
     );
   }
-}
-
-Widget eventItemBuilder(BuildContext context, int index) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 30, top: 10, bottom: 12, right: 20),
-    child: Row(
-      children: [
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "12",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w900,
-                color: Colors.grey[100],
-              ),
-            ),
-            Text(
-              "November",
-              style: TextStyle(
-                color: Colors.grey[100],
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Icon(
-              Icons.circle,
-              size: 10,
-              color: Colors.grey[100],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              "25",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w900,
-                color: Colors.grey[100],
-              ),
-            ),
-            Text(
-              "November",
-              style: TextStyle(
-                color: Colors.grey[100],
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Play Events At Tres Liverpool",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.grey[100],
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                "Tres Liverpool, Temple Court, Liverpool, UK",
-                style: TextStyle(
-                  color: Colors.grey[100],
-                ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Icon(
-                    Icons.location_on_outlined,
-                    size: 15,
-                    color: Colors.grey[100],
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    "2322 Avenue Vigor",
-                    style: TextStyle(
-                      color: Colors.grey[100],
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.schedule,
-                    size: 15,
-                    color: Colors.grey[100],
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    "2322 Avenue Vigor",
-                    style: TextStyle(
-                      color: Colors.grey[100],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
-      ],
-    ),
-  );
 }
---
+++ lib/features/guideHome/widgets/Events Calendar/singleEventItem.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class SingleEventItem extends StatefulWidget {
+  const SingleEventItem({Key? key}) : super(key: key);
+
+  @override
+  _SingleEventItemState createState() => _SingleEventItemState();
+}
+
+class _SingleEventItemState extends State<SingleEventItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, top: 10, bottom: 12, right: 20),
+      child: Row(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "12",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.grey[100],
+                ),
+              ),
+              Text(
+                "November",
+                style: TextStyle(
+                  color: Colors.grey[100],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Icon(
+                Icons.circle,
+                size: 10,
+                color: Colors.grey[100],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "25",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.grey[100],
+                ),
+              ),
+              Text(
+                "November",
+                style: TextStyle(
+                  color: Colors.grey[100],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Play Events At Tres Liverpool",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.grey[100],
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "Tres Liverpool, Temple Court, Liverpool, UK",
+                  style: TextStyle(
+                    color: Colors.grey[100],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      size: 15,
+                      color: Colors.grey[100],
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "2322 Avenue Vigor",
+                      style: TextStyle(
+                        color: Colors.grey[100],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.schedule,
+                      size: 15,
+                      color: Colors.grey[100],
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "2322 Avenue Vigor",
+                      style: TextStyle(
+                        color: Colors.grey[100],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
