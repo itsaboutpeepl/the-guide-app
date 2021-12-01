3b8af29b0a95d28ee374d5a4002d71bcb08d3465
Hussain Surti <surti.huss@gmail.com>
2021-12-01 16:50:11 +0400

    removed refresh flag, updated refreshList to use new action

--- lib/redux/viewsmodels/categoryArticleList.dart
+++ lib/redux/viewsmodels/categoryArticleList.dart
@@ -1,5 +1,4 @@
 import 'package:equatable/equatable.dart';
-import 'package:peepl/features/guideNews/screens/newsScreen.dart';
 import 'package:peepl/models/app_state.dart';
 import 'package:peepl/models/articles/blogArticle.dart';
 import 'package:peepl/redux/actions/news_actions.dart';
@@ -9,14 +8,12 @@ class CategoryArticleListViewModel extends Equatable {
   final String tabName;
   final String tabID;
   final List<BlogArticle> articles;
-  final bool isRefreshing;
   final void Function() refreshList;
 
   CategoryArticleListViewModel({
     required this.tabName,
     required this.tabID,
     required this.articles,
-    required this.isRefreshing,
     required this.refreshList,
   });
 
@@ -26,13 +23,11 @@ class CategoryArticleListViewModel extends Equatable {
         tabName: store.state.newsState.articles[currentTabIndex].categoryName,
         tabID: store.state.newsState.articles[currentTabIndex].categoryID,
         articles: store.state.newsState.articles[currentTabIndex].articleList,
-        isRefreshing: false,
         refreshList: () {
-          store.dispatch(UpdateIsRefresh(isRefreshing: true));
-          store.dispatch(updateCurrentTabList(query: getRandomQuery()));
+          store.dispatch(refreshCurrentTabList(query: "entertainment"));
         });
   }
 
   @override
-  List<Object> get props => [tabName, tabID, articles, isRefreshing];
+  List<Object> get props => [tabName, tabID, articles];
 }
