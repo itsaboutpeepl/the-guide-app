891fe30f97d3718c040e3162c4fe0e97a809eac8
Hussain Surti <surti.huss@gmail.com>
2021-12-01 14:55:26 +0400

    updated view for refresh indicator

--- lib/features/guideNews/widgets/categoryArticlesList.dart
+++ lib/features/guideNews/widgets/categoryArticlesList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:peepl/constants/theme.dart';
 import 'package:peepl/features/guideNews/screens/newsScreen.dart';
 import 'package:peepl/features/guideNews/widgets/singleCategoryArticle.dart';
 import 'package:peepl/features/guideNews/widgets/testCard.dart';
@@ -44,31 +45,37 @@ class _CategoryArticlesListState extends State<CategoryArticlesList> {
       builder: (_, vm) {
         return RefreshIndicator(
           onRefresh: () async {},
-          child: ListView.separated(
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-            controller: _scrollController,
-            itemCount: vm.articles.length,
-            itemBuilder: (_, int index) {
-              if (vm.articles.length - 1 == index) {
-                return Container(
-                  height: 50,
-                  child: Center(
-                    child: CircularProgressIndicator(),
+          child: !vm.isRefreshing
+              ? ListView.separated(
+                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  controller: _scrollController,
+                  itemCount: vm.articles.length,
+                  itemBuilder: (_, int index) {
+                    if (vm.articles.length - 1 == index) {
+                      return Container(
+                        height: 50,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                    return SingleCategoryArticle(
+                      article: vm.articles[index],
+                    );
+                  },
+                  separatorBuilder: (_, index) => Divider(
+                    indent: MediaQuery.of(context).size.width * 0.3,
+                    endIndent: MediaQuery.of(context).size.width * 0.3,
+                    height: 40,
+                    thickness: 2,
+                    color: Colors.grey[200],
                   ),
-                );
-              }
-              return SingleCategoryArticle(
-                article: vm.articles[index],
-              );
-            },
-            separatorBuilder: (_, index) => Divider(
-              indent: MediaQuery.of(context).size.width * 0.3,
-              endIndent: MediaQuery.of(context).size.width * 0.3,
-              height: 40,
-              thickness: 2,
-              color: Colors.grey[200],
-            ),
-          ),
+                )
+              : Center(
+                  child: CircularProgressIndicator(
+                    color: flexColorSchemeLight.primary,
+                  ),
+                ),
         );
       },
     );
