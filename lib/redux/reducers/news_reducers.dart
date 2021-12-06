2be3f157c8b44d1765f4821c89f15a1dbdcacb87
Hussain Surti <surti.huss@gmail.com>
2021-12-07 00:35:57 +0400

    refactor for new API

--- lib/redux/reducers/news_reducers.dart
+++ lib/redux/reducers/news_reducers.dart
@@ -5,18 +5,18 @@ import 'package:redux/redux.dart';
 final NewsStateReducers = combineReducers<NewsState>(
   [
     TypedReducer<NewsState, UpdateCategoryList>(_getCategoryList),
-    TypedReducer<NewsState, UpdateCategoryNames>(_getCategoryNames),
+    TypedReducer<NewsState, UpdateCategories>(_getCategories),
     TypedReducer<NewsState, UpdateCurrentTabList>(_getCurrentTabList),
     TypedReducer<NewsState, UpdateCurrentTabIndex>(_updateCurrentTabIndex),
     TypedReducer<NewsState, RefreshCurrentTabList>(_refreshCurrentTabList),
   ],
 );
 
-NewsState _getCategoryNames(
+NewsState _getCategories(
   NewsState state,
-  UpdateCategoryNames action,
+  UpdateCategories action,
 ) {
-  return state.copyWith(categoryNames: action.categoryNames);
+  return state.copyWith(categories: action.categories);
 }
 
 NewsState _getCategoryList(
