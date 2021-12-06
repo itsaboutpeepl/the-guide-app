825307f6df823bb96e167fa42021f91a493db533
Hussain Surti <surti.huss@gmail.com>
2021-12-07 00:35:24 +0400

    refactor for new class

--- lib/redux/viewsmodels/newsScreen.dart
+++ lib/redux/viewsmodels/newsScreen.dart
@@ -1,24 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:guide_liverpool/models/app_state.dart';
+import 'package:guide_liverpool/models/articles/category.dart';
 import 'package:guide_liverpool/models/articles/categoryArticles.dart';
 import 'package:redux/redux.dart';
 
 class NewsScreenViewModel extends Equatable {
-  final List<String> tabNames;
+  final List<Category> categories;
   final List<CategoryArticles> articles;
 
   NewsScreenViewModel({
-    required this.tabNames,
+    required this.categories,
     required this.articles,
   });
 
   static NewsScreenViewModel fromStore(Store<AppState> store) {
     return NewsScreenViewModel(
-      tabNames: store.state.newsState.categoryNames,
+      categories: store.state.newsState.categories,
       articles: store.state.newsState.articles,
     );
   }
 
   @override
-  List<Object> get props => [tabNames, articles];
+  List<Object> get props => [categories, articles];
 }
