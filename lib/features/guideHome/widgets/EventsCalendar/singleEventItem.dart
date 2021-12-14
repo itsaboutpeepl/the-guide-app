2e90ba96cf8c27d2fab1b4ba59d2f2a0230e62ff
Hussain Surti <surti.huss@gmail.com>
2021-12-14 16:00:45 +0400

    updated UI to fit model and added links for maps and booking

--- lib/features/guideHome/widgets/EventsCalendar/singleEventItem.dart
+++ lib/features/guideHome/widgets/EventsCalendar/singleEventItem.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:guide_liverpool/features/guideHome/helpers/UrlLaunch.dart';
 import 'package:guide_liverpool/models/articles/events.dart';
+import 'package:intl/intl.dart';
 
 class SingleEventItem extends StatefulWidget {
   final Events eventItem;
@@ -10,119 +12,165 @@ class SingleEventItem extends StatefulWidget {
 }
 
 class _SingleEventItemState extends State<SingleEventItem> {
+  DateFormat monthFormat = DateFormat.MMM();
+  DateFormat timeFormat = DateFormat.Hm();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 30, right: 20),
-      child: Row(
+      child: Stack(
         children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "12",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.grey[100],
-                ),
-              ),
-              Text(
-                "November",
-                style: TextStyle(
-                  color: Colors.grey[100],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Icon(
-                Icons.circle,
-                size: 10,
-                color: Colors.grey[100],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "25",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.grey[100],
-                ),
-              ),
-              Text(
-                "November",
-                style: TextStyle(
-                  color: Colors.grey[100],
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+          Positioned.fill(
+            child: Row(
               children: [
-                Text(
-                  "Play Events At Tres Liverpool",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.grey[100],
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "Tres Liverpool, Temple Court, Liverpool, UK",
-                  style: TextStyle(
-                    color: Colors.grey[100],
-                  ),
-                ),
-                SizedBox(height: 10),
-                Row(
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Icon(
-                      Icons.location_on_outlined,
-                      size: 15,
-                      color: Colors.grey[100],
-                    ),
-                    SizedBox(width: 5),
                     Text(
-                      "2322 Avenue Vigor",
+                      widget.eventItem.startDate.day.toString(),
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey[100],
+                      ),
+                    ),
+                    Text(
+                      monthFormat
+                          .format(widget.eventItem.startDate)
+                          .toUpperCase(),
                       style: TextStyle(
                         color: Colors.grey[100],
                       ),
-                    )
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Icon(
+                      Icons.circle,
+                      size: 10,
+                      color: Colors.grey[100],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      widget.eventItem.endDate.day.toString(),
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey[100],
+                      ),
+                    ),
+                    Text(
+                      monthFormat
+                          .format(widget.eventItem.endDate)
+                          .toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.grey[100],
+                      ),
+                    ),
                   ],
                 ),
                 SizedBox(
-                  height: 8,
+                  width: 20,
                 ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.schedule,
-                      size: 15,
-                      color: Colors.grey[100],
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      "2322 Avenue Vigor",
-                      style: TextStyle(
-                        color: Colors.grey[100],
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        parseHtmlString(widget.eventItem.eventTitle),
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.grey[100],
+                        ),
                       ),
-                    )
-                  ],
+                      SizedBox(height: 5),
+                      Text(
+                        parseHtmlString(widget.eventItem.description),
+                        maxLines: 3,
+                        style: TextStyle(
+                          color: Colors.grey[100],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: 15,
+                            color: Colors.grey[100],
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            widget.eventItem.location,
+                            style: TextStyle(
+                              color: Colors.grey[100],
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.schedule,
+                            size: 15,
+                            color: Colors.grey[100],
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            timeFormat.format(widget.eventItem.startDate) +
+                                " - " +
+                                timeFormat.format(widget.eventItem.endDate),
+                            style: TextStyle(
+                              color: Colors.grey[100],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
-          )
+          ),
+          Positioned(
+            bottom: 5,
+            right: -10,
+            child: IconButton(
+              icon: Icon(
+                Icons.location_pin,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                UrlLaunch.openMap(double.parse(widget.eventItem.latitude),
+                    double.parse(widget.eventItem.longitude));
+              },
+            ),
+          ),
+          widget.eventItem.bookingLink != ""
+              ? Positioned(
+                  bottom: 5,
+                  right: 25,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.launch,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      UrlLaunch.launchURL(widget.eventItem.bookingLink);
+                    },
+                  ),
+                )
+              : SizedBox.shrink()
         ],
       ),
     );
