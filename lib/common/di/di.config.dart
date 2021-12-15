64ec6fbb47c3f600f5838c6e0783530081427171
Hussain Surti <surti.huss@gmail.com>
2021-12-15 22:03:24 +0400

    generated file bump

--- lib/common/di/di.config.dart
+++ lib/common/di/di.config.dart
@@ -19,11 +19,11 @@ import '../../services/apis/explorer.dart' as _i5;
 import '../../services/apis/fuseswap.dart' as _i9;
 import '../../services/apis/market.dart' as _i12;
 import '../../services/apis/news.dart' as _i13;
-import '../../utils/log/log_it.dart' as _i16;
+import '../../utils/log/log_it.dart' as _i17;
 import '../../utils/onboard/Istrategy.dart' as _i10;
 import '../network/services.dart' as _i18;
 import '../network/web3.dart' as _i25;
-import '../router/routes.dart' as _i17;
+import '../router/routes.dart' as _i16;
 import 'dio.dart' as _i19;
 import 'firebase.dart' as _i20;
 import 'logger_di.dart' as _i22;
@@ -66,6 +66,7 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
   await gh.factoryAsync<_i14.PackageInfo>(() => packageInfoDi.packageInfo,
       preResolve: true);
   gh.lazySingleton<_i15.PhoneNumberUtil>(() => phone.phoneNumberUtil);
+  gh.singleton<_i16.RootRouter>(servicesModule.rootRouter);
   gh.factory<String>(() => web3Di.defaultCommunityAddress,
       instanceName: 'defaultCommunityAddress');
   gh.factoryParam<_i3.Web3, Map<dynamic, dynamic>?, dynamic>(
@@ -76,8 +77,7 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       (walletModules, _) => web3Di.ethereumWeb3(
           get<String>(instanceName: 'defaultCommunityAddress'), walletModules),
       instanceName: 'ethereumWeb3');
-  gh.lazySingleton<_i16.LogIt>(() => _i16.LogIt(get<_i11.Logger>()));
-  gh.singleton<_i17.RootRouter>(servicesModule.rootRouter);
+  gh.lazySingleton<_i17.LogIt>(() => _i17.LogIt(get<_i11.Logger>()));
   return get;
 }
 
