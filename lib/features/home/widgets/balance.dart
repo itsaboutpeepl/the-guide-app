5f6c626ea4af60c51096d35faff05cf6af689852
Hussain Surti <surti.huss@gmail.com>
2022-01-13 11:02:33 +0400

    added token help widget

--- lib/features/home/widgets/balance.dart
+++ lib/features/home/widgets/balance.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:guide_liverpool/features/guideHome/dialogs/tokenHelpDialog.dart';
 import 'package:guide_liverpool/generated/l10n.dart';
 import 'package:guide_liverpool/redux/viewsmodels/balance.dart';
 import 'package:guide_liverpool/models/app_state.dart';
@@ -14,99 +15,129 @@ class Balance extends StatelessWidget {
       distinct: true,
       converter: BalanceViewModel.fromStore,
       builder: (_, viewModel) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Row(
-              children: [
-                Flexible(
-                  child: Text(
-                    I10n.of(context).balance,
-                    style: TextStyle(
-                      color: Theme.of(context).canvasColor,
-                      fontSize: 13.0,
-                    ),
-                  ),
+        return Stack(
+          children: [
+            Positioned(
+              right: 0,
+              bottom: 0,
+              child: GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => TokenHelpDialog(),
+                  );
+                },
+                child: Icon(
+                  Icons.info_outline,
+                  color: Colors.white,
                 ),
-                SizedBox(
-                  width: 35,
-                ),
-                Flexible(
-                  child: Text(
-                    'Rewards',
-                    style: TextStyle(
-                      color: Theme.of(context).canvasColor,
-                      fontSize: 13.0,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-            Row(
-              children: [
-                AutoSizeText.rich(
-                  TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: viewModel.gbpxbalance,
-                      ),
-                      TextSpan(
-                        text: ' GBPX',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        I10n.of(context).balance,
                         style: TextStyle(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 14,
                           color: Theme.of(context).canvasColor,
+                          fontSize: 13.0,
                         ),
                       ),
-                    ],
-                  ),
-                  style: TextStyle(
-                    color: Theme.of(context).canvasColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                  ),
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                AutoSizeText.rich(
-                  TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: viewModel.pplBalance,
-                      ),
-                      TextSpan(
-                        text: ' PPL',
+                    ),
+                    SizedBox(
+                      width: 35,
+                    ),
+                    Flexible(
+                      child: Text(
+                        'Rewards',
                         style: TextStyle(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 14,
                           color: Theme.of(context).canvasColor,
+                          fontSize: 13.0,
                         ),
-                      )
-                    ],
-                  ),
-                  style: TextStyle(
-                    color: Theme.of(context).canvasColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                  ),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   width: 10,
+                    // ),
+                    // Flexible(
+                    //   child: Icon(
+                    //     Icons.help,
+                    //     color: Colors.white,
+                    //     size: 15,
+                    //   ),
+                    // ),
+                  ],
                 ),
+                Row(
+                  children: [
+                    AutoSizeText.rich(
+                      TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: viewModel.gbpxbalance,
+                          ),
+                          TextSpan(
+                            text: ' GBPX',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 14,
+                              color: Theme.of(context).canvasColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      style: TextStyle(
+                        color: Theme.of(context).canvasColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    AutoSizeText.rich(
+                      TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: viewModel.pplBalance,
+                          ),
+                          TextSpan(
+                            text: ' PPL',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 14,
+                              color: Theme.of(context).canvasColor,
+                            ),
+                          )
+                        ],
+                      ),
+                      style: TextStyle(
+                        color: Theme.of(context).canvasColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+                // Flexible(
+                //   child: AutoSizeText(
+                //     '\$${viewModel.usdValue}',
+                //     style: TextStyle(
+                //       fontWeight: FontWeight.bold,
+                //     ),
+                //     presetFontSizes: [
+                //       30,
+                //       25,
+                //       22,
+                //     ],
+                //   ),
+                // ),
               ],
             ),
-            // Flexible(
-            //   child: AutoSizeText(
-            //     '\$${viewModel.usdValue}',
-            //     style: TextStyle(
-            //       fontWeight: FontWeight.bold,
-            //     ),
-            //     presetFontSizes: [
-            //       30,
-            //       25,
-            //       22,
-            //     ],
-            //   ),
-            // ),
           ],
         );
       },
