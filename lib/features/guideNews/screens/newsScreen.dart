79bb37bfefecd39a19cd2b975136d566ede7c033
Hussain Surti <surti.huss@gmail.com>
2022-02-28 10:14:01 +0400

    added Loading indicator to newsScreen

--- lib/features/guideNews/screens/newsScreen.dart
+++ lib/features/guideNews/screens/newsScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:guide_liverpool/constants/theme.dart';
 import 'package:guide_liverpool/features/guideNews/widgets/categoryArticlesList.dart';
 import 'package:guide_liverpool/models/app_state.dart';
 import 'package:guide_liverpool/models/articles/category.dart';
@@ -25,8 +26,10 @@ class _NewsScreenState extends State<NewsScreen>
         _tabController.addListener(
           () {
             if (_tabController.indexIsChanging) {
+              store.dispatch(UpdateIsLoading(isLoading: true));
               store.dispatch(
                   UpdateCurrentTabIndex(currentTabIndex: _tabController.index));
+
               store.dispatch(
                 updateCurrentTabList(
                   query: store.state.newsState.categories[_tabController.index]
@@ -59,11 +62,18 @@ class _NewsScreenState extends State<NewsScreen>
             ],
           ),
         ),
-        body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            controller: _tabController,
-            children:
-                viewmodel.articles.map((e) => CategoryArticlesList()).toList()),
+        body: viewmodel.isLoading
+            ? Center(
+                child: CircularProgressIndicator(
+                    color: flexColorSchemeLight.primary),
+              )
+            : TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                controller: _tabController,
+                children: viewmodel.articles
+                    .map((e) => CategoryArticlesList())
+                    .toList(),
+              ),
       ),
     );
   }
