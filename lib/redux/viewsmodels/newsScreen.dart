9943b23f6f33fb68df8ddb06f175a6d1090a3982
Hussain Surti <surti.huss@gmail.com>
2022-02-28 10:13:44 +0400

    added loading to VM

--- lib/redux/viewsmodels/newsScreen.dart
+++ lib/redux/viewsmodels/newsScreen.dart
@@ -7,19 +7,22 @@ import 'package:redux/redux.dart';
 class NewsScreenViewModel extends Equatable {
   final List<Category> categories;
   final List<CategoryArticles> articles;
+  final bool isLoading;
 
   NewsScreenViewModel({
     required this.categories,
     required this.articles,
+    required this.isLoading,
   });
 
   static NewsScreenViewModel fromStore(Store<AppState> store) {
     return NewsScreenViewModel(
       categories: store.state.newsState.categories,
       articles: store.state.newsState.articles,
+      isLoading: store.state.newsState.isLoading,
     );
   }
 
   @override
-  List<Object> get props => [categories, articles];
+  List<Object> get props => [categories, articles, isLoading];
 }
