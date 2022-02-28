35e99be7698366598c83a233f4b69c33c5cd2660
Hussain Surti <surti.huss@gmail.com>
2022-02-28 13:06:26 +0400

    added demo account for reviews

--- lib/utils/onboard/sms.dart
+++ lib/utils/onboard/sms.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:guide_liverpool/common/router/routes.gr.dart';
 import 'package:guide_liverpool/constants/enums.dart';
 import 'package:guide_liverpool/redux/actions/user_actions.dart';
@@ -25,12 +26,19 @@ class SmsStrategy implements IOnBoardStrategy {
   Future verify(store, verificationCode, onSuccess) async {
     final String phoneNumber = store.state.userState.phoneNumber;
     final String accountAddress = store.state.userState.accountAddress;
-    final String jwtToken = await walletApi.verifySMS(
-      verificationCode,
-      phoneNumber,
-      accountAddress,
-      appName: Strings.appName,
-    );
+    String jwtToken = "";
+
+    if (phoneNumber == dotenv.env["DEMO_PHONE_NUMBER"]) {
+      jwtToken = dotenv.env["DEMO_JWT_TOKEN"]!;
+    } else {
+      jwtToken = await walletApi.verifySMS(
+        verificationCode,
+        phoneNumber,
+        accountAddress,
+        appName: Strings.appName,
+      );
+    }
+
     onSuccess(jwtToken);
   }
 }
