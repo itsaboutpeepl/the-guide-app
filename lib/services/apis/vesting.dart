96db3d1dcde23d846316750f55a71bc07c186481
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2022-07-14 01:16:22 +0100

    commented out credentials

--- lib/services/apis/vesting.dart
+++ lib/services/apis/vesting.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/io.dart';
 import '../../constants/urls.dart';
 import '../../models/vestingInfo/schedule.dart';
@@ -41,5 +42,18 @@ class VestingService {
         ContractAbi.fromJson(jsonEncode(jsonABI['abi']), 'VestingContract');
     _contractAddress =
         EthereumAddress.fromHex('0xfC75C482058d7f521Db493D103247953d5C9d2AF');
+  }
+
+  late EthPrivateKey _creds;
+  //TODO: need user priv key to sign transaction
+  // Future<void> getCredentials() async {
+  //   // _creds = EthPrivateKey.fromHex();
+  // }
+
+  late DeployedContract _deployedContract;
+  late ContractFunction _vestingSchedule;
+
+  Future<void> getDeployedContract() async {
+    _deployedContract = DeployedContract(_abiCode, _contractAddress);
   }
 }
