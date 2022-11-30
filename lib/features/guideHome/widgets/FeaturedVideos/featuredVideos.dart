cb4d742ca54f627cff4d968d618fcc7c3885fd4a
Hussain Surti <surti.huss@gmail.com>
2022-11-30 04:11:59 +0400

    added check for empty videos

--- lib/features/guideHome/widgets/FeaturedVideos/featuredVideos.dart
+++ lib/features/guideHome/widgets/FeaturedVideos/featuredVideos.dart
@@ -18,38 +18,42 @@ class _FeaturedVideosState extends State<FeaturedVideos> {
       distinct: true,
       converter: FeaturedVideosViewModel.fromStore,
       builder: (_, viewmodel) {
-        return SliverPadding(
-          padding: EdgeInsets.all(15),
-          sliver: SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Watch",
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+        return viewmodel.featuredVideos.isEmpty
+            ? SliverToBoxAdapter(child: SizedBox.shrink())
+            : SliverPadding(
+                padding: EdgeInsets.all(15),
+                sliver: SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Watch",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Divider(
+                        thickness: 3,
+                        endIndent: MediaQuery.of(context).size.width * 0.8,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        child: PageView.builder(
+                            physics: PageScrollPhysics(),
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (_, index) => SingleFeaturedVideo(
+                                videoArticleItem:
+                                    viewmodel.featuredVideos[index]),
+                            itemCount: viewmodel.featuredVideos.length),
+                      )
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  height: 5,
-                ),
-                Divider(
-                  thickness: 3,
-                  endIndent: MediaQuery.of(context).size.width * 0.8,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  child: PageView.builder(
-                      physics: PageScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (_, index) => SingleFeaturedVideo(
-                          videoArticleItem: viewmodel.featuredVideos[index]),
-                      itemCount: viewmodel.featuredVideos.length),
-                )
-              ],
-            ),
-          ),
-        );
+              );
       },
     );
   }
